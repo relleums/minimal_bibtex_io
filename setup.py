3ca9ca262519d55c114fea2e9b6b3dd2bfd37279
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-07-08 14:39:30 +0200

    oder fields as in PyPi guide

--- setup.py
+++ setup.py
@@ -7,20 +7,14 @@ with open("README.rst", "r", encoding="utf-8") as f:
 setuptools.setup(
     name="minimal_bibtex_io_relleums",
     version="0.0.2",
+    author="Sebastian Achim Mueller",
+    author_email="sebastian-achim.mueller@mpi-hd.mpg.de",
     description="A minimal loader and dumper for bibtex-files",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     url="https://github.com/relleums/minimal_bibtex_io",
     project_urls={
         "Bug Tracker": "https://github.com/relleums/minimal_bibtex_io/issues",
-    },
-    author="Sebastian Achim Mueller",
-    author_email="sebastian-achim.mueller@mpi-hd.mpg.de",
-    packages=["minimal_bibtex_io",],
-    package_data={
-        "minimal_bibtex_io": [
-            os.path.join("tests", "resources", "example.bib")
-        ],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -30,5 +24,11 @@ setuptools.setup(
         "Topic :: Text Processing",
         "Topic :: Text Processing :: Markup :: LaTeX",
     ],
+    packages=["minimal_bibtex_io",],
+    package_data={
+        "minimal_bibtex_io": [
+            os.path.join("tests", "resources", "example.bib")
+        ],
+    },
     python_requires=">=3.0",
 )
